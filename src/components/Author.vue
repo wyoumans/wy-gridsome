aadd9bb01ec0b58c04ef613f68bb61900a0f878b
Will <will@williamyoumans.com>
2019-04-30 23:10:43 -0400

    whitespace

--- src/components/Author.vue
+++ src/components/Author.vue
@@ -1,24 +1,24 @@
 <template>
-	<div class="author">
+  <div class="author">
 
-		<g-image alt="Author image" class="author__image" src="~/assets/images/author.jpg" width="180" height="180" blur="5" />
+    <g-image alt="Author image" class="author__image" src="~/assets/images/author.jpg" width="180" height="180" blur="5" />
 
-		<h1 v-if="showTitle" class="author__site-title">
-			{{ $static.metaData.siteName }}
-		</h1>
+    <h1 v-if="showTitle" class="author__site-title">
+      {{ $static.metaData.siteName }}
+    </h1>
 
-		<p class="author__intro">
-			{{ $static.metaData.siteDescription}}<br>
-			Founder of <a href="https://www.forefrontsoftware.com">Forefront Software</a> and <a href="https://www.ctosumo.com">CTO Sumo</a>
+    <p class="author__intro">
+      {{ $static.metaData.siteDescription}}<br>
+      Founder of <a href="https://www.forefrontsoftware.com">Forefront Software</a> and <a href="https://www.ctosumo.com">CTO Sumo</a>
 
-		</p>
+    </p>
 
-		<p class="author__links">
-			<a href="//twitter.com/wyoumansdev">Follow on Twitter</a>
-			<a href="//github.com/wyoumans">GitHub</a>
-		</p>
+    <p class="author__links">
+      <a href="//twitter.com/wyoumansdev">Follow on Twitter</a>
+      <a href="//github.com/wyoumans">GitHub</a>
+    </p>
 
-	</div>
+  </div>
 </template>
 
 <static-query>
@@ -31,37 +31,37 @@ query {
 
 <script>
 export default {
-	props: ['showTitle']
+  props: ['showTitle']
 }
 </script>
 
 <style lang="scss">
 .author {
-	margin: 0 auto;
-	max-width: 500px;
-	text-align: center;
-	padding: calc(var(--space) / 2) 0;
+  margin: 0 auto;
+  max-width: 500px;
+  text-align: center;
+  padding: calc(var(--space) / 2) 0;
 
-	&__image {
-		border-radius: 100%;
-		width: 90px;
-		height: 90px;
-		margin-bottom: 1em;
-	}
+  &__image {
+    border-radius: 100%;
+    width: 90px;
+    height: 90px;
+    margin-bottom: 1em;
+  }
 
-	&__intro {
-		opacity: .8;
-	}
+  &__intro {
+    opacity: .8;
+  }
 
-	&__site-title {
-		font-size: 1.5em;
-	}
+  &__site-title {
+    font-size: 1.5em;
+  }
 
-	&__links {
-		margin-top: -.5em;
-		a {
-			margin: 0 .5em;
-		}
-	}
+  &__links {
+    margin-top: -.5em;
+    a {
+      margin: 0 .5em;
+    }
+  }
 }
 </style>
